fcb211f778509f16cbbd4f95ea4943fe4216228e
thanhdat163 <148530513+thanhdat163@users.noreply.github.com>
2024-01-03 20:46:35 +0700

    window programming

---
+++ Lession4/Lesson4/Exercise1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;     
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = e.Uri.AbsoluteUri,
+                    UseShellExecute = true
+                });
+                e.Handled = true; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Lession8/Lession8/Exercise 2/WebForm.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise_2
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebService webService = new WebService();
+            int a = 3, b=6;
+            Label1.Text = "Output of WebService: " + webService.Add(a, b).ToString();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            WebService webService = new WebService();
+            Single A = 6, B = 3;
+            Label2.Text = "Output of WebService: " + webService.Subtract(A, B).ToString();
+        }
+    }
+}
